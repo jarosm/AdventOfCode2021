5e99b8aace46d77415185ab6a341d73dd4eee232
Martin Jaros <jarosm@centrum.cz>
2021-12-01 06:13:31 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace AdventOfCode2021
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] input = File.ReadAllLines("input.txt");
+
+			Console.WriteLine("end");
+			Console.ReadLine();
+		}
+	}
+}
